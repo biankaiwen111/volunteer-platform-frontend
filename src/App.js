350f2e293cdc143a763bda19d0f0543366e7afba
biankaiwen111 <biankaiwen111@outlook.com>
2021-08-17 05:40:40 -0700

    feat: main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import "./App.css";
+import $ from "jquery";
+import { useEffect, useState, Fragment } from "react";
 
 function App() {
-  return null;
+  const [display, setDisplay] = useState(true);
+
+  useEffect(() => {
+    $("#form").css("display", "none");
+  }, []);
+
+  const clickEnterHandler = (event) => {
+    $("#form").css("display", "block");
+    setDisplay(false);
+  };
+
+  const clickleaveHandler = (event) => {
+    $("#form").css("display", "None");
+    setDisplay(true);
+  };
+
+  return (
+    <div>
+      {display ? (
+        <Fragment>
+          <h1>Welcome to the transferMe volunteer platform!</h1>
+          <input
+            type="button"
+            value="Let's Get Started!"
+            onClick={clickEnterHandler}
+          />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <input
+            type="button"
+            value="Back to home page"
+            onClick={clickleaveHandler}
+          />
+        </Fragment>
+      )}
+    </div>
+  );
 }
 
 export default App;
